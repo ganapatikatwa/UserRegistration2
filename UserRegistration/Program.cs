c99a0ff2f1d6fae74b99f06eee28d70f03c4107c
Ganapati <ganapatasakatwa@gmail.com>
2023-06-19 05:35:51 +0530

    [GK] UC1 Valid First Name

---
+++ UserRegistration/Program.cs
@@ -0,0 +1,32 @@
+﻿using Regex_UserRegistration;
+using System;
+
+namespace REGEX_UserRegistration
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to REGEX User Registration Problem");
+
+            Console.WriteLine("Select Options Please :\n" +
+                "1)Enter First Name\n");
+
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+
+                    UserRegistration user = new UserRegistration();
+                    user.ValidFirstName();
+                    break;
+
+                default:
+                    Console.WriteLine("Choose Proper options");
+                    break;
+            }
+        }
+    }
+}
---
+++ UserRegistration/UserRegistration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Regex_UserRegistration
+{
+    public class UserRegistration
+    {
+        public void ValidFirstName()
+        {
+            Console.WriteLine("Please Enter First Name");
+            string data = Console.ReadLine();
+            string pattern = "^[A-Z[a-z]{2,10}$";
+
+            Regex regobj = new Regex(pattern);
+            if(regobj.IsMatch(data))
+            {
+                Console.ForegroundColor= ConsoleColor.Green;
+                Console.WriteLine("First Name is Valid");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor= ConsoleColor.Red;
+                Console.WriteLine("First Name Is Invalid");
+                Console.ResetColor();
+            }
+        }
+    }
+}
